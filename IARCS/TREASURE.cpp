10785127be0e6a14b67edde39831b14f84d40291
kuntalmz <mynamekuntal@gmail.com>
2016-12-20 12:34:58 +0530

    Add files via upload

--- IARCS/TREASURE.cpp
+++ IARCS/TREASURE.cpp
@@ -3,11 +3,12 @@
 
 int main(){
     int machines , tokens , start , end;
+    int total;
     std::cin >> machines >> tokens >> start >> end;
-    start--;//end--;
+    total = machines * tokens;
+    start--;end--;
     std::vector<std::vector<int> >tokenTable(machines,std::vector<int>(tokens));
     std::vector<std::vector<int> >machTable(machines,std::vector<int>(tokens));
-    std::vector<std::vector<bool> >visited(machines,std::vector<bool>(tokens,false));
     std::vector<int>visitedMach;
     for(int i=0;i<machines;i++){
         for(int j=0;j<tokens;j++){
@@ -19,23 +20,39 @@ int main(){
         }
     }
 
-    int moves =0 ,token = 0;
+    int token = 0;
 
-    while(moves < end){
-        if(visited[start][token]){
-            int modFac = end%(moves+1);
-            //modFac = modFac + visited[start][token];
-            std::cout << visited[start][token] << ' ' << moves <<' ' <<modFac<<std::endl;
-            std::cout <<visitedMach[modFac]+1<< ' ' << visitedMach[modFac-1] << ' ' << visitedMach[modFac+1] << std::endl;
-            return 0;
+    // If there are a lot of moves, then find a cycle first and then reduce
+    // the number of moves to something less than the cycle length.
+    if (end > total * 3) {
+        // First, make M*T moves.
+        for (int moves = 0; moves < total; moves++) {
+            int newTok = tokenTable[start][token];
+            int newStart = machTable[start][token];
+            token = newTok;
+            start = newStart;
         }
-        visited[start][token] = true;
-        visitedMach.push_back(start);
+        // Next, count the number of moves until we reach this spot again.
+        int cycleStart = start;
+        int cycleToken = token;
+        int cycleLength = 0;
+        do {
+            int newTok = tokenTable[start][token];
+            int newStart = machTable[start][token];
+            token = newTok;
+            start = newStart;
+            cycleLength++;
+        } while (start != cycleStart || token != cycleToken);
+        // Now, we can reduce the number of total moves.
+        end = (end - total) % cycleLength;
+    }
+
+    // Now make moves until we reach the end.
+    for (int moves = 0; moves < end; moves++) {
         int newTok = tokenTable[start][token];
         int newStart = machTable[start][token];
         token = newTok;
         start = newStart;
-        moves++;
     }
     std::cout << start+1 << std::endl;
     return 0;
