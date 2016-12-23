c91b1b27760f6a108e22ee9c62b961b99678a5a9
kuntalmz <mynamekuntal@gmail.com>
2016-12-23 18:22:50 +0530

    Add files via upload

--- IARCS/CATERCONT.cpp
+++ IARCS/CATERCONT.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
-struct conn{
-    int from;
-    int to;
+//A Red Black Tree Problem
+//Red means , the node is chosen and black the opposite
+//if we choose a node we cant choose its children or better say neighbours
+//if we dont choose one we can choose the neighbours as well as turn black as well , whatever is the max
+int n;
+std::vector<int>ifRed(100000);
+std::vector<int>ifBlack(100000);
+std::vector<bool>visited(100000);
+
+struct adjacent{
+    std::vector<int>verts;
+    int size = 0;
 };
 
+void dfs(int vertex,const std::vector<adjacent> &graph){
+    visited[vertex] = true;
+    int size = graph[vertex].size;
+    for(int i=0;i<size;i++){
+        if(!visited[graph[vertex].verts[i]]){
+            dfs(graph[vertex].verts[i],graph);
+            ifRed[vertex] += ifBlack[graph[vertex].verts[i]]; //If choose it , we cant choose the adjacent vertexes
+            int temp = ifRed[graph[vertex].verts[i]];
+            if(ifBlack[graph[vertex].verts[i]] > temp){
+                temp = ifBlack[graph[vertex].verts[i]];
+            }
+            ifBlack[vertex] += temp;
+        }
+    }
+}
+
 int main(){
-    int n;
     std::cin >> n;
-    std::vector<int>weights(n);
-    std::vector<conn>nextVert(n);
+    std::vector<adjacent>graph(n);
     for(int i=0;i<n;i++){
-        std::cin >> weights[i];
-    }
-    for(int i=0;i<n;i++){
-        std::cin >> nextVert[i].from >> nextVert[i].to;
-        nextVert[i].from--;nextVert[i].to--;
+        std::cin >> ifRed[i];
     }
     
-    
+    for(int i=0;i<n-1;i++){
+        int a ,b;
+        std::cin >> a >> b;
+        a--;b--;
+        graph[a].verts.push_back(b);
+        graph[a].size++;
+        graph[b].verts.push_back(a);
+        graph[b].size++;
+    }
+    dfs(0,graph);
+    int sum = ifRed[0];
+    if(ifBlack[0] > sum){
+        sum = ifBlack[0];
+    }
+    std::cout << sum << std::endl;
     return 0;
 }
