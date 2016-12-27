450754d04231499577f3808054dddd65e3ce698e
kuntalmz <mynamekuntal@gmail.com>
2016-12-27 16:49:03 +0530

    Add files via upload

--- IARCS/EXPT.cpp
+++ IARCS/EXPT.cpp
@@ -1,11 +1,10 @@
-//Still Incomplete
-
 #include <iostream>
 #include <vector>
 
 
 int main(){
     int n;
+    std::ios_base::sync_with_stdio(false);
     std::cin >> n;
     std::vector<int>first(n);
     std::vector<int>second(n);
@@ -18,25 +17,25 @@ int main(){
     }
     
     std::vector<std::vector<int> >table(n,std::vector<int>(n,1));
-    std::vector<int>parentTableFirst(n,-1);
-    std::vector<int>parentTableSec(n,-1);    
+    std::vector<std::vector<int> >parentTableFirst(n,std::vector<int>(n,-1));
+    std::vector<std::vector<int> >parentTableSec(n,std::vector<int>(n,-1));    
     
     int max = 0;
     int x , y;
     //std::cout << "got here" << std::endl;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=n-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
             //std::cout << "got here" << std::endl;
-            for(int k=i+1;k<n;k++){
-                for(int l=j+1;l<n;l++){
+            for(int k=i-1;k>=0;k--){
+                for(int l=j-1;l>=0;l--){
                     //std::cout << k << ' ' << l << std::endl;
                     if(first[k]-first[i] == second[l]-second[j]){
-                        if(table[i][j] <= table[k][l]){
+                        if(table[k][l] <= table[i][j] ){
                             table[k][l] = table[i][j] + 1;
-                            if(table[k][l] > max){
+                            parentTableFirst[k][l] = i;
+                            parentTableSec[k][l] = j;
+                            if (table[k][l] > max ||(table[k][l] == max && (k < x ||(k == x && l < y)))) {
                                 max = table[k][l];
-                                parentTableFirst[k] = i;
-                                parentTableSec[l] = j;
                                 x = k;
                                 y = l;
                             }
@@ -47,37 +46,39 @@ int main(){
         }
     }
     
+    
     std::cout << max << std::endl;
     
-    std::vector<int>sequenceFirst;
-    std::vector<int>sequenceSec;
+    int xCopy = x;
+    int yCopy = y;
     
+    for(int i=0;i<max;i++){
+        int temp = xCopy;
+        std::cout << first[xCopy] << ' ';
+        xCopy = parentTableFirst[xCopy][yCopy];
+        yCopy = parentTableSec[temp][yCopy];
+    }
+    std::cout << std::endl;
+    
+    for(int i=0;i<max;i++){
+        int temp = x;
+        std::cout << second[y] << ' ';
+        x = parentTableFirst[x][y];
+        y = parentTableSec[temp][y];
+    }
+    std::cout << std::endl;
+    /*
     while(parentTableFirst[x] != -1){
-        sequenceFirst.push_back(first[x]);
-        //std::cout << first[x] << std::endl;
+        std::cout << first[x] << ' ';
         x = parentTableFirst[x];
     }
-    sequenceFirst.push_back(first[x]);
-    //std::cout << first[x];
-    
-    for(int i=max-1;i>=0;i--){
-        std::cout << sequenceFirst[i] << ' ';
-    }
-    
-    std::cout << std::endl;
-    
+    std::cout << first[x] << std::endl;
     
     while(parentTableSec[y] != -1){
-        sequenceSec.push_back(second[y]);
+        std::cout << second[y] << ' ';
         y = parentTableSec[y];
     }
-    sequenceSec.push_back(second[y]);
-    
-    for(int i=max-1;i>=0;i--){
-        std::cout << sequenceSec[i] << ' ';
-    }
-    
-    std::cout << std::endl;
+    std::cout << second[y] << std::endl;*/
     
     return 0;
 }
