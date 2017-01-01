0a863fba24931b81d580a6a47c6dbf3e9eaa4ad9
kuntalmz <mynamekuntal@gmail.com>
2017-01-01 16:27:14 +0530

    Add files via upload

---
+++ INOI/HIGHWAY.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+#define MOD 20011
+
+int main(){
+    int n,m,d;
+    std::cin >> n >> m >> d;
+    std::vector<std::vector<int> >fromTop(n,std::vector<int>(m,0));
+    std::vector<std::vector<int> >fromLeft(n,std::vector<int>(m,0));
+    std::vector<std::vector<int> >ifPossible(n,std::vector<int>(m,0));
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            std::cin >> ifPossible[i][j];
+        }
+    }
+    
+    fromTop[0][0] = 1;
+    fromLeft[0][0] = 1;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if((i==0 && j==0) || !ifPossible[i][j]){
+                continue;
+            }
+            int ways = 0;
+            //Algorithm:
+            //fromLeft[i][j] = fromTop[i-1][j] + fromTop[i-2][j] +....+ fromTop[i-d][j]
+            //fromTop [i][j] = fromLeft[i][j-1] + fromLeft[i][j-2] +....+ fromLeft[i][j-d]
+            int min = i-d;
+            if(min < 0){
+                min = 0;
+            }
+            for(int k=i-1; k>=min;k--){
+                if(!ifPossible[k][j]){
+                    break;
+                }
+                ways += fromTop[k][j];
+            }
+            fromLeft[i][j] = ways%MOD;
+            ways = 0;
+            min = j-d;
+            if(min < 0){
+                min = 0;
+            }
+            for(int k=j-1;k>=min;k--){
+                if(!ifPossible[i][k]){
+                    break;
+                }
+                ways += fromLeft[i][k];
+            }
+            fromTop[i][j] = ways%MOD;
+        }
+    }
+    std::cout << (fromLeft[n-1][m-1] + fromTop[n-1][m-1])%MOD << std::endl;
+    return 0;
+}
